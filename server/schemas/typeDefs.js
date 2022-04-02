1d6777c9bc99c4d65c5c785d3effae94f0ea3777
Alexander Lanz <austinalexanderlanz@gmail.com>
2022-04-01 21:20:28 -0400

    set up typeDefs and Resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,25 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
- type Book {
-   _id: ID!
-   authors: [String]
-   description: String!
-   bookId: String!
-   image: String
-   link: String
-   title: String!
- }
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
- type User {
-   _id: ID!
-   username: String!
-   email: String!
-   password: String!
-   savedBooks: [Book]
- }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
 
- type Auth {
+  type Auth {
     token: ID!
     user: User
   }
@@ -29,17 +29,21 @@ const typeDefs = gql`
   }
 
   input NewBook {
-   authors: [String]
-   description: String!
-   bookId: String!
-   image: String
-   link: String
-   title: String!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
   }
 
   type Mutation {
     createUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+    login(email: String, username: String, password: String!): Auth
     saveBook(newBook: NewBook!): User
     deleteBook(bookId: ID!): User
   }
---
+++ server/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation saveBook($newBook: NewBook) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks { 
+          bookId
+          authors
+          description
+          image
+          link
+          title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation deleteBook($bookId: ID!) {
+    deleteBook($bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks { 
+          bookId
+          authors
+          description
+          image
+          link
+          title
+      }
+    }
+  }
+`;
